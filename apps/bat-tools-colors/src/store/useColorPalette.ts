9283253836eab8994aed8f10cd723f062951a0a9
BatDev64 <bat.bits64@gmail.com>
2024-08-30 17:35:58 -0300

    feat(apps/bat-tools-colors): add selectedPalettes state and actions

--- apps/bat-tools-colors/src/store/useColorPalette.ts
+++ apps/bat-tools-colors/src/store/useColorPalette.ts
@@ -5,16 +5,26 @@ import { generateColorScale } from '../utils/generateColorScale'
 import { generateColorPalette } from '../utils/generateColorPalette'
 import chroma from 'chroma-js'
 
-interface State {
-  palettes: Palette[]
+interface PalettesActions {
   createPalette: (color?: string) => void
   removePalette: (id: string) => void
   editPalette: (id: string, newColor: string) => void
 }
+interface SavedPalettesActions {
+  savePalette: (id: string) => void
+  unsavePalette: (id: string) => void
+}
 
-const storeApi: StateCreator<State, [['zustand/devtools', never]]> = (set, get) => {
+type Action = PalettesActions & SavedPalettesActions
+interface State {
+  palettes: Palette[]
+  selectedPalettes: Palette[]
+}
+
+const storeApi: StateCreator<State & Action, [['zustand/devtools', never]]> = (set, get) => {
   return {
     palettes: [],
+    selectedPalettes: [],
     createPalette: (color?: string) => {
       const randomColor = color || chroma.random().hex()
       const palette = generateColorPalette(randomColor)
@@ -25,6 +35,7 @@ const storeApi: StateCreator<State, [['zustand/devtools', never]]> = (set, get) 
       const newPalettes = palettes.filter((el) => el.id !== id)
       set({ palettes: newPalettes }, false, 'REMOVE_PALETTE')
     },
+
     editPalette: (id: string, newColor: string) => {
       const { palettes } = get()
       const oldPalettes = structuredClone(palettes)
@@ -47,11 +58,32 @@ const storeApi: StateCreator<State, [['zustand/devtools', never]]> = (set, get) 
         false,
         'EDIT_PALETTE'
       )
+    },
+    /* selected palette */
+    savePalette: (id: string) => {
+      const { palettes } = get()
+      const cloneState = structuredClone(palettes)
+      const indexPalette = cloneState.findIndex((palette) => palette.id === id)
+      const newStatePalettes = cloneState.filter((palette) => palette.id !== id)
+      set(
+        (state) => ({
+          selectedPalettes: [...state.selectedPalettes, cloneState[indexPalette]],
+          palettes: newStatePalettes
+        }),
+        false,
+        'SAVE_PALETTE'
+      )
+    },
+    unsavePalette: (id: string) => {
+      const { selectedPalettes: savedPalettes } = get()
+      const cloneState = structuredClone(savedPalettes)
+      const newPalettes = cloneState.filter((el) => el.id !== id)
+      set({ selectedPalettes: newPalettes }, false, 'UNSAVE_PALETTE')
     }
   }
 }
 
-export const useColorPaletteStore = create<State>()(
+export const useColorPaletteStore = create<State & Action>()(
   devtools(
     persist(storeApi, {
       name: 'store-palette',
