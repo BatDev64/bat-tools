083b9a1661eca024f765d0510a0cb5a3c12a405f
BatDev64 <bat.bits64@gmail.com>
2024-09-12 09:00:29 -0300

    feat(apps/bat-tools-colors): add component Main in App

--- apps/bat-tools-colors/src/App.tsx
+++ apps/bat-tools-colors/src/App.tsx
@@ -1,41 +1,27 @@
-import { PaletteCard } from './components/PaletteCard/PaletteCard'
-import { Button } from './components/ui/Button/Button'
-import { Add } from './components/Icons/Add'
-import { usePalettes } from './Hooks/storeHooks/usePalettes'
-import { useCreatePalette } from './Hooks/storeHooks/useCreatePalette'
+import { Main } from './components/Main/Main'
 
+/* 1edc34  489cea*/
+
+
+/* console.log(gCs('#489cea')) */
 function App() {
-  const palettes = usePalettes()
-  const createPalette = useCreatePalette()
-
   return (
-    <div className='wrapper'>
+    <div className='wrapper flex flex-col'>
       <header className='text-center'>
         <h1 className='scale-text-lg font-bold uppercase'>Bat tools color palette generator</h1>
       </header>
-      <section className='relative flex flex-col gap-4'>
-        <ul className='flex flex-col gap-8 '>
-          {palettes.map((palette) => {
-            return (
-              <li key={palette.id}>
-                <PaletteCard colorPalette={palette} />
-              </li>
-            )
-          })}
-        </ul>
-        <div className='sticky inset-0 flex justify-center py-2'>
-          <Button
-            onClick={() => createPalette()}
-            className='p-4 rounded-md bg-slate-50 text-indigo-700 shadow-xl'>
-            Add color
-            <span>
-              <Add className='size-6 fill-indigo-700' />
-            </span>
-          </Button>
-        </div>
-      </section>
+
+      <Main />
     </div>
   )
 }
 
 export default App
+/*       <PaletteCard colorPalette={{
+        id: '1',
+        "current-color": '#489cea',
+        isSaved: false,
+        shades: P
+      }}>
+
+      </PaletteCard> */
