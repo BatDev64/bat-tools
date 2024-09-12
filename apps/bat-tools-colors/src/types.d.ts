b9020a424dbedb0a2d035679a8e9522c17ce2997
BatDev64 <bat.bits64@gmail.com>
2024-09-12 09:03:48 -0300

    refactor(apps/bat-tools-colors): modify types and create new type

--- apps/bat-tools-colors/src/types.d.ts
+++ apps/bat-tools-colors/src/types.d.ts
@@ -1,7 +1,27 @@
-type Shades = Record<string | number, string>
+/* type Shades = Record<string | number, string> */
+/*export type Shades = {
+  50: string
+  100: string
+  200: string
+  300: string
+  400: string
+  500: string
+  600: string
+  700: string
+  800: string
+  900: string
+  950: string
+} */
+
+export type Shades = {
+  [key in 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950]: string
+}
 
 export interface Palette {
   id: string
   'current-color': string
   shades: Shades
+}
+export interface PaletteType extends Palette {
+  isSaved: boolean
 }
