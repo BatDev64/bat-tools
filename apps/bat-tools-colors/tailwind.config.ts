66bf2cf169844f8e94a6cc48d46fde8baceb8476
BatDev64 <bat.bits64@gmail.com>
2024-09-12 09:05:07 -0300

    feat(apps/bat-tools-colors): add brand colors

--- apps/bat-tools-colors/tailwind.config.ts
+++ apps/bat-tools-colors/tailwind.config.ts
@@ -3,7 +3,36 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['src/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'royal-blue': {
+          '50': '#eef2ff',
+          '100': '#e0e7ff',
+          '200': '#c7d2fe',
+          '300': '#a5b5fc',
+          '400': '#818df8',
+          '500': '#686cf2',
+          '600': '#4d45e6',
+          '700': '#4237cb',
+          '800': '#3630a3',
+          '900': '#302e81',
+          '950': '#1e1b4b'
+        },
+        logan: {
+          '50': '#f4f5f9',
+          '100': '#eceef3',
+          '200': '#dbdfea',
+          '300': '#c5cbdc',
+          '400': '#adb3cc',
+          '500': '#9fa3c1',
+          '600': '#8183aa',
+          '700': '#6e6f94',
+          '800': '#5b5d78',
+          '900': '#4d4e62',
+          '950': '#2d2d39'
+        }
+      }
+    }
   },
   plugins: []
 } satisfies Config
