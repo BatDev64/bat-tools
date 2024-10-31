34c87055e984cc1b6ad0b7dc975e6a5408e479c0
BatDev64 <bat.bits64@gmail.com>
2024-10-31 16:59:17 -0300

    feat(apps/bat-colors): add layout styles

--- apps/bat-colors/src/App.tsx
+++ apps/bat-colors/src/App.tsx
@@ -2,6 +2,7 @@ import { colorScale } from '@bat-tools/colors'
 import { type ColorScale } from './models/types'
 import { ColorList } from './components/ColorList/ColorList'
 import { ColorSquare } from './components'
+import styles from './App.module.css'
 
 const defaultScales: ColorScale = colorScale('8683a5')
 const nameSacale = Object.keys(defaultScales)[0]
@@ -10,16 +11,23 @@ const shades = defaultScales[nameSacale]
 function App() {
   return (
     <div className='wrapper'>
-      <h1>Bat Colors</h1>
-      <ColorSquare color={shades[500]} />
-      <ColorList
-        shades={shades}
-        nameColor={nameSacale}
-      />
+      <section className={styles.layout}>
+        <header className={styles.header}>
+          <h1>Bat Colors</h1>
+        </header>
+        <main className={styles.main}>
+          <ColorSquare color={shades[500]} />
+          <ColorList
+            shades={shades}
+            nameColor={nameSacale}
+          />
 
-      <pre>
-        <code>{JSON.stringify(defaultScales, null, 2)}</code>
-      </pre>
+          <pre>
+            <code>{JSON.stringify(defaultScales, null, 2)}</code>
+          </pre>
+        </main>
+        <footer className={styles.footer}></footer>
+      </section>
     </div>
   )
 }
