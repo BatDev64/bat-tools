97aa65039fd0d8c4c84fcf0e652d013660593494
BatDev64 <bat.bits64@gmail.com>
2024-10-31 16:23:10 -0300

    feat(apps/bat-colors): show color scale

--- apps/bat-colors/src/App.tsx
+++ apps/bat-colors/src/App.tsx
@@ -1,8 +1,26 @@
+import { colorScale } from '@bat-tools/colors'
+import { type ColorScale } from './models/types'
+import { ColorList } from './components/ColorList/ColorList'
+import { ColorSquare } from './components'
+
+const defaultScales: ColorScale = colorScale('8683a5')
+const nameSacale = Object.keys(defaultScales)[0]
+const shades = defaultScales[nameSacale]
+
 function App() {
   return (
-    <>
+    <div className='wrapper'>
       <h1>Bat Colors</h1>
-    </>
+      <ColorSquare color={shades[500]} />
+      <ColorList
+        shades={shades}
+        nameColor={nameSacale}
+      />
+
+      <pre>
+        <code>{JSON.stringify(defaultScales, null, 2)}</code>
+      </pre>
+    </div>
   )
 }
 
