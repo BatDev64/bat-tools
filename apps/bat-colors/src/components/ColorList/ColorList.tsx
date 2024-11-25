af77988cdca37125fcefc615e31a84d9356fc457
BatDev64 <bat.bits64@gmail.com>
2024-11-25 20:58:38 -0300

    refactor(apps/bat-colors): simplify colorList

--- apps/bat-colors/src/components/ColorList/ColorList.tsx
+++ apps/bat-colors/src/components/ColorList/ColorList.tsx
@@ -1,28 +1,33 @@
-import { type Shades } from '../../models/types'
-import { ColorSquare } from '../ColorSquare/ColorSquare'
+import { type ColorScaleWithId } from '../../models/types'
+import SquareIcon from '../Icons/Square'
 import styles from './ColorList.module.css'
 
-interface Props {
-  shades: Shades
-  nameColor: string
-}
+type Props = ColorScaleWithId
 
-export const ColorList = ({ shades, nameColor }: Props) => {
+export const ColorList = ({ shades, name, id }: Props) => {
   const colors = Object.entries(shades)
 
   return (
-    <ul className={styles.container}>
-      {colors.map(([shade, color]) => {
-        return (
-          <li key={`${nameColor}-${color}-${shade}`}>
-            <ColorSquare
-              color={color}
-              shade={shade}
-              showLabel
-            />{' '}
-          </li>
-        )
-      })}
-    </ul>
+    <div className={styles.container}>
+      <h3 className={styles.title}>{name}</h3>
+      <ul className={styles.container}>
+        {colors.map(([shade, color]) => {
+          return (
+            <li key={`${id}-${shade}`}>
+              <div className={styles.color}>
+                <div className={styles['color-square']}>
+                  <SquareIcon
+                    fill={color}
+                    className={styles.svg}
+                  />
+                  <p>{color}</p>
+                </div>
+                <p>{shade}</p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
