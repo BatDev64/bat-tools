cfa5dbaccb9d2222f3ecbd33c8baf653a511c9e5
BatDev64 <bat.bits64@gmail.com>
2024-11-25 21:54:02 -0300

    refactor(apps/bat-colors): change types and output function

--- packages/bat-tools-colors/src/colorSacle.ts
+++ packages/bat-tools-colors/src/colorSacle.ts
@@ -3,7 +3,8 @@ import namer from 'color-namer'
 import { type ColorScale } from './models/colorSacle.types'
 import { sortByLuminance, createShades, isColorHex } from './utils'
 
-const luminances = [10, 15, 20, 35, 45, 55, 65, 78, 90, 96, 98]
+// const luminances = [10, 15, 20, 35, 45, 55, 65, 78, 90, 96, 98]
+const luminances = [5, 12, 20, 30, 42, 55, 68, 80, 90, 95, 98]
 
 export const colorScale = (color: string) => {
   if (typeof color !== 'string') {
@@ -17,7 +18,7 @@ export const colorScale = (color: string) => {
   }
 
   const colors = luminances.map((el, index) => {
-    const saturationAdjustment = index > 6 ? 0.8 : 1
+    const saturationAdjustment = index < 6 ? 0.8 : 1
     return chroma(color)
       .set('hsl.l', el / 100)
       .set('hsl.s', chroma(color).get('hsl.s') * saturationAdjustment)
@@ -25,11 +26,15 @@ export const colorScale = (color: string) => {
   })
 
   const sortedColors = sortByLuminance(colors)
-
-  const nameColor = namer(color).pantone[0].name.toLowerCase().replace(' ', '-')
+  const shades = createShades(sortedColors)
+  const nameColor = namer(shades[500])
+    .pantone[0].name.toLowerCase()
+    .replace(' ', '-')
 
   const scale: ColorScale = {
-    [nameColor]: createShades(sortedColors)
+    name: nameColor,
+
+    shades: shades
   }
 
   return scale
