d3952fa78da6bb69aa9063c8ee2d591288c96cdd
BatDev64 <bat.bits64@gmail.com>
2024-11-25 21:19:16 -0300

    feat(apps/bat-colors): add type ColorScaleWithId

--- apps/bat-colors/src/models/types.d.ts
+++ apps/bat-colors/src/models/types.d.ts
@@ -1,3 +1,10 @@
 export type Shade = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 950
 export type Shades = Record<Shade, string>
-export type ColorScale = Record<string, Shades>
+export type ColorScale = {
+  name: string
+  shades: Shades
+}
+
+export interface ColorScaleWithId extends ColorScale {
+  id: string
+}
