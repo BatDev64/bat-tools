d5203b49b7bb67562de12844a14bb85ce7168af0
BatDev64 <bat.bits64@gmail.com>
2025-02-18 17:00:15 -0300

    feat(apps/bat-palette): show PaletteList

--- apps/bat-palette/src/App.tsx
+++ apps/bat-palette/src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from '#components/Header/Header'
 import { usePalette } from '#hooks/usePalette'
 import { useCustomProperties } from '#hooks/useCustomProperties'
+import { PaletteList } from '#components/App/PaletteList.tsx'
 
 function App() {
   const { selectedColors } = usePalette()
@@ -15,6 +16,11 @@ function App() {
       <div className='wrapper mt-8'>
         <Header />
       </div>
+      <main className='flex flex-col gap-4 mt-8 relative'>
+        <div className='wrapper w-full flex flex-col gap-16'>
+          <PaletteList />
+        </div>
+      </main>
     </div>
   )
 }
