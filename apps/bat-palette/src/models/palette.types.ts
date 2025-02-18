df50778c92e68bb74c947ed52406ac2256dff39f
BatDev64 <bat.bits64@gmail.com>
2025-02-18 15:48:00 -0300

    feat(apps/bat-palette): add Palette types

---
+++ apps/bat-palette/src/models/palette.types.ts
@@ -0,0 +1,17 @@
+export type Format = 'hex' | 'rgb' | 'hsl' | 'oklch'
+export type FormatsWithoutOKLCH = Exclude<Format, 'oklch'>
+
+export type Step =
+  `${50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950}`
+
+export type PaletteSteps = Record<Step, string>
+
+export interface Palette {
+  name: string
+  colorScales: Record<FormatsWithoutOKLCH, PaletteSteps>
+  cssVariables: Record<Format, string>
+}
+
+export type ColorCategory = 'primary' | 'secondary' | 'neutral'
+
+export type SelectedColors = Record<ColorCategory, Palette>
